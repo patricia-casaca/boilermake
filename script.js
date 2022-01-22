5fca349fb4ef99ec76d1965fd2878d67438ba1b0
angela-joseph-r <70658485+angela-joseph-r@users.noreply.github.com>
2022-01-22 16:00:41 -0500

    added another fortune

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function fortuneMessage() {
     var items = ["You will have a good day",
                     "You will have a bad day",
-                "You will have an ok day"];
+                "You will have an ok day", "You will have an amazing day!"];
     var item = items[Math.random() * items.length];
 }
