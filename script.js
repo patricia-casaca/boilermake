6e0870da47aded0d302c8c039a2b4dc9593903a3
Weiyi <66911094+wytan01@users.noreply.github.com>
2022-01-22 15:55:15 -0500

    files for fortune teller website

---
+++ script.js
@@ -0,0 +1,6 @@
+function fortuneMessage() {
+    var items = ["You will have a good day",
+                    "You will have a bad day",
+                "You will have an ok day"];
+    var item = items[Math.random() * items.length];
+}
